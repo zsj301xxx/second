5bff4b6fb3c071729fc7fff7d6aa564146af67c7
zsj301xxx <1223575347@qq.com>
2019-05-24 21:03:52 +0800

    ss

---
+++ js/order.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    var myTab = document.getElementById("tab");    //整个div
+    var myUl = myTab.getElementsByTagName("ul")[0];//一个节点
+    var myLi = myUl.getElementsByTagName("li");    //数组
+    var myDiv = document.querySelectorAll("#tab>div");
+
+    for(var i = 0; i<myLi.length;i++){
+        myLi[i].index = i;
+        myLi[i].onclick = function(){
+            for(var j = 0; j < myLi.length; j++){
+                myLi[j].className="off";
+                myDiv[j].className = "hide";
+            }
+            this.className = "on";
+            myDiv[this.index].className = "show";
+        }
+    }
+}
